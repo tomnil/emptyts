19bab525b07d8bf9f2dd1fb79a85972c0f2f98ae
Tomas Nilsson <none@none>
2023-03-26 21:27:00 +0200

    Added documentation and debugger example

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,48 @@
 import Express from "express";
 
-// Use global variables
+// ***************************************************
+// * Use global variables (see global.d.ts file for definitions)
+// ***************************************************
 
 global.Config = { Foo: "Bar" };
 global.Foo = "Bar";
 
-// Setup express
+// ***************************************************
+// * Setup express
+// ***************************************************
 
 const app = Express();
 
+// ***************************************************
+// * Add a couple of routes
+// ***************************************************
+
 app.get('/ping', function (req: Express.Request, res: Express.Response) {
-    console.log(`${new Date().toISOString()}: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    // req.UserID is available here because of the modification of the global namespace
-    res.json({ Pong: new Date() });
+	console.log(`${new Date().toISOString()}: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+	// req.UserID is available here because of the modification of the global namespace
+	res.json({ Pong: new Date() });
 });
 
-app.listen(4000, () => console.log('Listening on port 4000! Try http://localhost:4000/ping'));
+app.get('/attach-to-debugger', function (req: Express.Request, res: Express.Response) {
 
+	console.log("The debugger will auto attach here if run with for example `npm run tsc-watch`.");
+	debugger; // Use debugger statement or F9 to set breakpoints
+	console.log("Step to me by pressing F10");
+	res.json({ DebuggingCompleted: new Date() });
+});
 
-export {
+// ***************************************************
+// * Start listening on port 4000
+// ***************************************************
 
-};
+app.listen(4000, () => {
+	console.log('Listening on port 4000. Try these URL:s');
+	console.log("http://localhost:4000/ping");
+	console.log("http://localhost:4000/attach-to-debugger");
+});
+
+// ***************************************************
+// * This is mandatory in all files
+// ***************************************************
+
+export { };
